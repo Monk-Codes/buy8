e01bd407f667d7dd1fca415b75e3f4d3a02726e4
monk-codes <your_donotreply_email_id>
2024-06-11 11:53:56 +0530

    product card refactor

--- src/components/HomePageProductCard.jsx
+++ src/components/HomePageProductCard.jsx
@@ -38,35 +38,33 @@ const HomePageProductCard = () => {
  }, [cartItems]);
 
  return (
-  <div className="mt-6 px-4 md:px-8 ">
+  <div className="mt-4 px-4 md:px-8 ">
    {/* Heading  */}
-   <div>
-    <h1 className="text-center mb-1 text-3xl font-button text-amber-500 hover:scale-x-110 transition ease-in-out duration-300 hover:text-amber-300 shadow-md ">Bestselling Products</h1>
+   <div className="flex w-full justify-center">
+    <h1 className="w-1/3 text-center mb-1 text-3xl font-button text-amber-500 hover:scale-x-110 transition ease-in-out duration-300 hover:text-amber-300 shadow-md ">Bestselling Products</h1>
    </div>
 
    {/* main  */}
-   <section className="text-gray-600 body-font">
-    <div className="container mx-auto">
-     <div className="flex flex-wrap -m-4">
+   <section className="text-gray-600 body-font h-full">
+    <div className="container px-1 lg:px-0 py-5 mx-auto">
+     <div className="flex flex-wrap justify-center">
       {getAllProduct.slice(0, 8).map((item, index) => {
        const { id, title, price, productImage, category } = item;
        return (
-        <div key={index} className="p-8 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-         <div className="h-full border border-gray-300 rounded-xl px-4 overflow-hidden shadow-md cursor-pointer hover:shadow-lg transition-shadow duration-300">
-          <div className="p-4 flex flex-1 justify-center">
-           <img className="h-48 object-cover object-center  " src={productImage} alt="product" onClick={() => navigate(`/productinfo/${id}`)} />
-          </div>
-          <div className="p-6">
+        <div key={index} className="px-2 mb-3 w-64 h-96 sm:w-1/2 md:w-1/3 lg:w-1/5">
+         <div className="product-card border border-gray-400 rounded-2xl overflow-hidden shadow-md cursor-pointer">
+          <img className="h-56 w-full p-6 transition duration-300 ease-in-out transform hover:scale-105" src={productImage} alt="product" onClick={() => navigate(`/productinfo/${id}`)} />
+          <div className="p-4 transition duration-300 ease-in-out transform hover:bg-gray-200">
            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{category}</h2>
-           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{title.substring(0, 25)}</h1>
-           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">₹{price}</h1>
+           <h1 className="title-font text-lg font-medium text-gray-700 mb-3">{title.substring(0, 25)}</h1>
+           <h1 className="title-font text-lg font-main text-amber-500 mb-3">₹{price}</h1>
            <div className="flex justify-center">
             {cartItems.some((e) => e.id === item.id) ? (
-             <button onClick={() => deleteCart(item)} className="bg-amber-500 hover:bg-orange-500 focus:bg-orange-700 w-full text-white py-2 rounded-lg font-bold focus:outline-none focus:ring-2 focus:ring-pink-300 transition-colors duration-300">
+             <button onClick={() => deleteCart(item)} className="bg-amber-500 hover:bg-orange-500 w-full text-white py-[4px] rounded-2xl font-bold focus:outline-none focus:ring focus:ring-offset-2 focus:ring-amber-500">
               Remove
              </button>
             ) : (
-             <button onClick={() => addCart(item)} className="bg-amber-500 hover:bg-orange-500 focus:bg-orange-700 w-full text-white py-2 rounded-lg font-main focus:outline-none focus:ring-2 focus:ring-pink-300 transition-colors duration-300">
+             <button onClick={() => addCart(item)} className="bg-slate-500 hover:bg-amber-400 w-full text-white py-[4px] rounded-2xl font-bold focus:outline-none focus:ring focus:ring-offset-2 focus:ring-amber-500">
               Add To Cart
              </button>
             )}
--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -37,29 +37,29 @@ const AllProduct = () => {
 
  return (
   <Layout>
-   <div className=" min-h-screen">
+   <div className=" min-h-screen px-4 mt-4 md:px-8">
     {/* Heading  */}
-    <div className="">
-     <h1 className=" text-center mb-5 text-2xl font-semibold">All Products</h1>
+    <div className="flex justify-center w-full">
+     <h1 className="w-1/3 text-center mb-1 text-3xl font-button text-amber-500 hover:scale-x-110 transition ease-in-out duration-300 hover:text-amber-300 shadow-md ">All Products</h1>
     </div>
 
     {/* main  */}
     <section className="text-gray-600 body-font h-full">
-     <div className="container px-5 lg:px-0 py-5 mx-auto">
-      <div className="flex flex-wrap">
-       {getAllProduct.map((item, index) => {
+     <div className="container px-1 lg:px-0 py-5 mx-auto">
+      <div className="flex flex-wrap justify-center">
+       {getAllProduct.slice(0, 8).map((item, index) => {
         const { id, title, price, productImage, category } = item;
         return (
-         <div key={index} className="px-10 mb-4 w-full md:w-1/2 lg:w-1/4">
-          <div className="product-card h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
-           <img onClick={() => navigate(`/productinfo/${id}`)} className="h-56 w-full lg:w-full p-10 transition duration-300 ease-in-out transform hover:scale-105" src={productImage} alt="image" />
+         <div key={index} className="px-2 mb-3 h-96 w-64 sm:w-1/2 md:w-1/3 lg:w-1/5">
+          <div className="product-card border border-gray-400 rounded-2xl overflow-hidden shadow-md cursor-pointer">
+           <img onClick={() => navigate(`/productinfo/${id}`)} className="h-56 w-full p-6 transition duration-300 ease-in-out transform hover:scale-105" src={productImage} alt="image" />
            <div className="p-4 transition duration-300 ease-in-out transform hover:bg-gray-200">
             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{category}</h2>
             <h1 className="title-font text-lg font-medium text-gray-700 mb-3">{title.substring(0, 25)}</h1>
-            <h1 className="title-font text-lg font-medium text-amber-500 mb-3">₹{price}</h1>
+            <h1 className="title-font text-lg font-main text-amber-500 mb-3">₹{price}</h1>
             <div className="flex justify-center ">
              {cartItems.some((p) => p.id === item.id) ? (
-              <button onClick={() => deleteCart(item)} className="bg-slate-500 hover:bg-amber-400 w-full text-white py-[4px] rounded-2xl font-bold focus:outline-none focus:ring focus:ring-offset-2 focus:ring-amber-500">
+              <button onClick={() => deleteCart(item)} className="bg-amber-500 hover:bg-orange-500 w-full text-white py-[4px] rounded-2xl font-bold focus:outline-none focus:ring focus:ring-offset-2 focus:ring-amber-500">
                Remove
               </button>
              ) : (
